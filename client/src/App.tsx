ff7ae2b0b4f6ce0841168ef1df875e9ca0245f72
Sushil Bhandari <zoroat3@gmail.com>
2024-04-20 12:17:57 +0100

    Got the access token, ready to be used

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,42 @@
+import { useEffect, useState } from "react"
+
 function App() {
+
+  useEffect(()=>{
+    const code = new URLSearchParams(window.location.search).get('code')
+    if(code !== null){
+      getTokenFromCode(code);
+    }
+    getUserAccounts();
+  },[])
+  
+  const getTokenFromCode = async(authorizationCode:string) => {
+    try{
+      const response = await fetch(`http://localhost:5000/getToken?code=${authorizationCode}`)
+      if(response.ok){
+        const data = await response.json();
+        sessionStorage.setItem("accessToken",data.access_token);
+        sessionStorage.setItem("refreshToken",data.refresh_token);
+      }
+
+    }catch(error){
+      console.log(`Error getting the accessToken ${error}`)
+    }
+
+    
+  }
+
+  const getUserAccounts = async() => {
+    try{
+      const response = await fetch(`http://localhost:5000/getAccounts?accessToken=${sessionStorage.getItem('accessToken')}`);
+      if(response.ok){
+        console.log(response);
+      }
+
+    }catch(error){
+      console.log(error);
+    }
+  }
 
   return (
     <h1 className="text-3xl font-bold underline">
--- server/app.js
+++ server/app.js
@@ -1,14 +1,85 @@
-import express from 'express'
-
+const express = require('express');
+const cors = require('cors')
+const axios = require('axios')
 const app = express();
 const port = process.env.PORT || "5000"
+require('dotenv').config();
 app.use(express.json());
+app.use(cors());
 
+const liveClientID = process.env.LIVE_CLIENT_ID;
+const liveClientSecret = process.env.LIVE_CLIENT_SECRET;
 
-app.get("/",(req,res)=>{
-    res.send("Hello from the Express Server")
+const sandboxClientID = process.env.SANDBOX_CLIENT_ID;
+const sandboxClientSecret = process.env.SANDBOX_CLIENT_SECRET;
+
+const redirectURI = process.env.REDIRECT_URI;
+const frontEndURI = "http://localhost:3000/"
+
+const liveRedirectPath = "https://auth.truelayer.com/?response_type=code&client_id=pennytrack-fe3de0&scope=info%20accounts%20balance%20cards%20transactions%20direct_debits%20standing_orders%20offline_access&redirect_uri=http://localhost:3000/callback&providers=uk-cs-mock%20uk-ob-all%20uk-oauth-all"
+const sandboxRedirectPath = "https://auth.truelayer-sandbox.com/?response_type=code&client_id=sandbox-pennytrack-fe3de0&scope=info%20accounts%20balance%20cards%20transactions%20direct_debits%20standing_orders%20offline_access&redirect_uri=http://localhost:5000/callback&providers=uk-cs-mock%20uk-ob-all%20uk-oauth-all"
+
+app.get("/login", (req,res) => {
+    try{
+        res.redirect(liveRedirectPath)
+    } catch (error){
+        console.log(`Error ${error}`)
+    }
 })
 
+app.get("/getToken", async (req,res) => {
+    const authCode = req.query.code
+    
+    try{
+        const options = {
+            method: 'POST',
+            url: 'https://auth.truelayer.com/connect/token',
+            headers: {
+                accept: 'application/json', 
+                'content-type': 'application/json'
+            },
+            data: {
+                grant_type: 'authorization_code',
+                client_id: liveClientID,
+                client_secret: liveClientSecret,
+                code: authCode,
+                redirect_uri: "http://localhost:3000/callback"
+            }
+          };
+
+        const response = await axios.request(options);
+        
+        res.json(response.data)
+
+
+    }catch (error){
+        console.log(`Error on callback ${error}`)
+    }
+
+})
+
+
+app.get("/getAccounts", async(req,res)=>{
+
+    const accessToken = req.query.accessToken;
+
+    const options = {
+        method:"GET",
+        url: "https://api.truelayer.com/data/v1/accounts",
+        headers:{
+            accept:'application/json',
+            authorization: `Bearer ${accessToken}`
+        }
+    }
+
+    try{
+        const response = await axios.request(options);
+        console.log(response);
+    }catch(error){
+        console.log(`Couldn't get user accounts: ${error}`)
+        res.send(error);
+    }
+})
 
 app.listen(port, () => {
     `Server Running at port: ${port}`
